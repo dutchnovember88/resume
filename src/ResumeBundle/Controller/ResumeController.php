e812a540fcbd3144f16bab7cab4f3dfd56c0cdce
Howard Noz <dutchnovember88@gmail.com>
2016-02-03 18:16:10 -0800

    change route

--- src/ResumeBundle/Controller/ResumeController.php
+++ src/ResumeBundle/Controller/ResumeController.php
@@ -5,6 +5,9 @@ namespace ResumeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
+/**
+ * @Route("/resume")
+ */
 class ResumeController extends Controller
 {
     /**
