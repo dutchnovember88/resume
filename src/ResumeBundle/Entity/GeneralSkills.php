a2bbe41a49dd5a171a9e7d008e32d3296485bf55
Howard Noz <dutchnovember88@gmail.com>
2016-02-03 18:14:16 -0800

    add general skills

---
+++ src/ResumeBundle/Entity/GeneralSkills.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace ResumeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * GeneralSkills
+ *
+ * @ORM\Table(name="general_skills")
+ * @ORM\Entity(repositoryClass="ResumeBundle\Repository\GeneralSkillsRepository")
+ */
+class GeneralSkills
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="level", type="string", length=255)
+     */
+    private $level;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return GeneralSkills
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set level
+     *
+     * @param string $level
+     *
+     * @return GeneralSkills
+     */
+    public function setLevel($level)
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    /**
+     * Get level
+     *
+     * @return string
+     */
+    public function getLevel()
+    {
+        return $this->level;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return GeneralSkills
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
+
