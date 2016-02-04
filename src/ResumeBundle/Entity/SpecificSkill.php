95ca44d5b626f82ba55375a975aad0db0ecbafb8
Howard Noz <dutchnovember88@gmail.com>
2016-02-03 19:46:31 -0800

    generate specificskill entity

---
+++ src/ResumeBundle/Entity/SpecificSkill.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace ResumeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SpecificSkill
+ *
+ * @ORM\Table(name="specific_skill")
+ * @ORM\Entity(repositoryClass="ResumeBundle\Repository\SpecificSkillRepository")
+ */
+class SpecificSkill
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="category", type="string", length=255)
+     */
+    private $category;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return SpecificSkill
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set category
+     *
+     * @param string $category
+     *
+     * @return SpecificSkill
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get category
+     *
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+}
+
